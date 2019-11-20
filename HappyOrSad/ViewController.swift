8b1d5c96b7b08011a166ec82af7f84728c0a0e31
Keira Hartley <khartley@lcs.on.ca>
2019-11-20 10:18:41 -0500

    Finished Storyboard and main screen + created properties from the labels.

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -10,11 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var labelOutput: UILabel!
+    @IBOutlet weak var emojiText: UITextField!
+    
+    //MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func calculateHappiness(_ sender: Any) {
+    }
+    
 
 }
 
