f441aa23a5f875f778bcf5dbe8d644bfbc558814
Keira Hartley <khartley@lcs.on.ca>
2020-01-24 22:34:26 -0500

    Happy or Sad now calculates the proper result.

--- HappyOrSad/ViewController.swift
+++ HappyOrSad/ViewController.swift
@@ -23,6 +23,11 @@ class ViewController: UIViewController {
 
     //Analyze the text
     @IBAction func calculateHappiness(_ sender: Any) {
+        // Clear output label from the last time the button was pressed
+        labelOutput.text = ""
+        
+        var happyCount = 0
+        var sadCount = 0
         
         //Guard against no input
         guard let emojiPhrase = emojiText.text, emojiPhrase.count > 0 else {
@@ -35,9 +40,32 @@ class ViewController: UIViewController {
             labelOutput.text = "Please enter a message with no more than 255 characters."
             return
         }
-       
         
+        //Make constants for the emojis
+        let happyEmojis = ("😀😃😄😁😆😅😂🤣☺️😊🙂😌😋😛😝😜😚😍🥰😘😗😙🤪🤩🥳")
+        let sadEmojis = ("😒😞😔😟😕🙁☹️😣😖😫😩🥺😢😭😨😰😥😓😪😿")
+        
+        
+        //Run through the phrase to find emojis
+        for character in emojiPhrase {
+            if happyEmojis.contains(character){
+               happyCount = happyCount + 1
+            }
+            
+            if sadEmojis.contains(character){
+                sadCount = sadCount + 1
+            }
         }
         
+        if happyCount > sadCount {
+            labelOutput.text = "They are more happy than sad."
+        }else if happyCount < sadCount {
+            labelOutput.text = "They are more sad than happy."
+        }else {
+            labelOutput.text = "They are neither happy or sad."
+        }
+    
+        }
+    
         
     }
